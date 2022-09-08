a482a384ef832de6d5cacef72d81cf6563f59df2
aleshkiyle <aleksey.rodin62@gmail.com>
2022-09-09 01:35:40 +0300

    implement threads from metanit

---
+++ src/main/java/com/rodin/concurrency/metanit/thread/MyThread.java
@@ -0,0 +1,15 @@
+package com.rodin.concurrency.metanit.thread;
+
+public class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s finished... \n", Thread.currentThread().getName());
+    }
+}
